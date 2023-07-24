0b43d5fe3ad6f069b10c3f21c9ab99934d14915f
Karl Mose <karl.h.mose@gmail.com>
2023-07-24 18:41:30 +0200

    stavefejl

--- raytracer_1/raytracer_1_functions.py
+++ raytracer_1/raytracer_1_functions.py
@@ -1,11 +1,11 @@
 from linalg_functions.linalg_functions import *
 from linalg_functions.colours import *
 
-def screen_pixel_corrdinate_to_3d_point(x, y, SCREEN_TOP_LEFT, SCREEN_X_VECTOR, SCREEN_Y_VECTOR):
+def screen_pixel_coordinate_to_3d_point(x, y, SCREEN_TOP_LEFT, SCREEN_X_VECTOR, SCREEN_Y_VECTOR):
     return 0
 
 def compute_ray(camera_p, pixel_p):
-    return (0, 0)
+        return (0, 0)
 
 def compute_distance_to_obj(ray, obj):
     if "Sphere" in type(obj).__name__:
