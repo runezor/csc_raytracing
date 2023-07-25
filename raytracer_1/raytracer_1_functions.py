bb5021db8ba3b614b6fe38636b00ca2edb7250bd
Karl Mose <karl.h.mose@gmail.com>
2023-07-25 18:07:27 +0200

    Fix stubs

--- raytracer_1/raytracer_1_functions.py
+++ raytracer_1/raytracer_1_functions.py
@@ -2,10 +2,10 @@ from linalg_functions.linalg_functions import *
 from linalg_functions.colours import *
 
 def screen_pixel_coordinate_to_3d_point(x, y, SCREEN_TOP_LEFT, SCREEN_X_VECTOR, SCREEN_Y_VECTOR):
-    return SCREEN_TOP_LEFT+SCREEN_X_VECTOR*x+SCREEN_Y_VECTOR*y
+    return np.array([0,0,0])
 
 def compute_ray(camera_p, pixel_p):
-    return (camera_p, normalised(pixel_p-camera_p))
+    return (np.array([0,0,0]), np.array([0,0,0]))
 
 def compute_distance_to_obj(ray, obj):
     if "Sphere" in type(obj).__name__:
